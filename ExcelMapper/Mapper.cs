146cca6ad7e7989fbf99549924c6c99dd0d1c59f
omer1abay <abayomer1905@gmail.com>
2024-03-11 21:05:37 +0300

    nullable type check added

--- ExcelMapper/Mapper.cs
+++ ExcelMapper/Mapper.cs
@@ -4,20 +4,39 @@
     {
         public static TDestination Map(List<string> source, List<string> columns)
         {
-            var destination = Activator.CreateInstance<TDestination>();
-            var columnsAndSource = source.Zip(columns, (s, c) => new { Source = s, Column = c });
-            foreach (var item in columnsAndSource)
+            try
             {
-                var destinationProperties = typeof(TDestination).GetProperty(item.Column);
-                var destinationType = typeof(TDestination).GetProperty(item.Column)!.PropertyType;
-                if (destinationProperties != null)
+                var destination = Activator.CreateInstance<TDestination>();
+                var columnsAndSource = source.Zip(columns, (s, c) => new { Source = s, Column = c });
+                foreach (var item in columnsAndSource)
                 {
-                    var tempValue = Convert.ChangeType(item.Source, destinationType);
-                    destinationProperties.SetValue(destination, tempValue);
-                }
-            }
+                    var destinationProperties = typeof(TDestination).GetProperty(item.Column);
+                    var destinationType = typeof(TDestination).GetProperty(item.Column)?.PropertyType;
+                    if (destinationProperties != null)
+                    {
 
-            return destination;
+                        if (destinationType.IsGenericType && destinationType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                        {
+                            if (item == null)
+                            {
+                                return destination;
+                            }
+
+                            destinationType = Nullable.GetUnderlyingType(destinationType);
+                        }
+
+                        var tempValue = Convert.ChangeType(item.Source, destinationType);
+                        destinationProperties.SetValue(destination, tempValue);
+                    }
+                }
+
+                return destination;
+            }
+            catch (Exception)
+            {
+
+                return Activator.CreateInstance<TDestination>();
+            }
         }
     }
 }
